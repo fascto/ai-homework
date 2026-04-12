f713203697f7ac469fad60a64c8ee90c0e35d9f9
Fascto <fascto.05@gmail.com>
2026-04-11 21:22:08 -0300

    feat(printing): adding the print method and using it in main for debugging

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 
 #include <iostream>
 #include <ostream>
+#include "./include/graph.h"
 
 int main() {
 
     std::cout << "ElPepe!" << std::endl;
+
+    Graph<int> graph{true, false};
+
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addVertex(2);
+
+    graph.addEdge(0, 1, 12);
+    graph.addEdge(0, 2, 3);
+    graph.addEdge(1, 2, 5);
+
+    graph.printGraph();
 
 }
