775e01ebfd48d7ad075f27b57cda4477a4f19f48
Fascto <fascto.05@gmail.com>
2026-05-10 19:33:12 -0300

    feat(MLP): Implementing backpropagation core and adding another things to MLP.h

---
+++ include/ml/MLP.h
@@ -0,0 +1,116 @@
+//
+// Created by tdela on 23/4/2026.
+//
+
+#ifndef IAHOMEWORK_MLP_H
+#define IAHOMEWORK_MLP_H
+#include <random>
+#include <vector>
+
+#include "metrics.h"
+#include "../math/calculus.h"
+#include "../math/linear_algebra/Matrix.h"
+
+class MLP {
+
+    std::size_t m_layers{};
+    std::size_t m_npl{};
+
+    math::linear_algebra::Matrix m_feature_matrix{};
+    std::vector<float> m_weights{};
+    ml::activations::ActivationFunction m_activation_function{};
+    ml::metrics::LossFunction m_loss_function{};
+    float m_learning_rate{};
+
+    std::vector<float> m_labels{};
+
+    void initWeights() {
+        const auto num_features = m_feature_matrix.getCols();
+
+        m_weights.resize(num_features, 0);
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution dis(0.f, 1.f);
+
+        for (int i = 0; i < num_features; ++i) {
+            m_weights[i] = dis(gen);
+        }
+    }
+
+public:
+
+    explicit MLP (
+        const math::linear_algebra::Matrix& feature_matrix,
+        const std::vector<float>& labels,
+        const std::size_t layers = 3,
+        const std::size_t npl = 3,
+        const float learning_rate = 0.1f,
+        const ml::activations::ActivationFunction activation_function = ml::activations::ActivationFunction::RELU,
+        const ml::metrics::LossFunction loss_function = ml::metrics::LossFunction::MSE
+        )
+    : m_layers(layers), m_npl(npl), m_activation_function(activation_function), m_loss_function(loss_function), m_learning_rate(learning_rate) {
+
+        m_learning_rate = learning_rate;
+        m_feature_matrix = feature_matrix;
+        m_labels = labels;
+
+        initWeights();
+    }
+
+    void train(const int& epochs=100) {
+
+        for (int i = 0; i < epochs; i++) {
+            const auto gradients = backpropagation(m_loss_function,m_labels);
+
+            const auto updated_weights = math::calculus::gradient_descent(m_weights, gradients, m_learning_rate);
+            m_weights = updated_weights;
+        }
+    }
+
+    void forward() {
+
+    }
+
+    [[nodiscard]] std::vector<float> backpropagation(
+        ml::metrics::LossFunction const loss_function,
+        std::vector<float> y_real
+        ) const {
+
+        auto weights = this->m_weights;
+        auto x = this->m_feature_matrix;
+
+        std::vector<float> gradients{};
+        std::vector<float> avg_gradients{};
+
+        avg_gradients.resize(weights.size(), 0.f);
+
+        // Run gradient descent
+        for (int s = 0; s < y_real.size(); s++) {
+
+            for (int j = 0; j < weights.size(); j++) {
+                auto loss = [&x, &y_real, &weights, loss_function, j, s, this](const float w_j) {
+                    float z{0.0f};
+                    for (int k = 0; k < weights.size(); k++) {
+                        float w_k;
+                        if (k == j)
+                            w_k = w_j;
+                        else
+                            w_k = weights[k];
+
+                        z += w_k * x.get(k, s);
+                    }
+                    const float y_pred = ml::activations::apply(m_activation_function, z);
+                    return ml::metrics::apply(loss_function, std::vector<float>{y_pred}, std::vector<float>{y_real[s]});
+                };
+                const auto gradient = math::calculus::evaluate(math::types::function { .func = loss , .order = 1 }, weights[j]);
+                gradients.push_back(gradient);
+                avg_gradients[j] += gradient/static_cast<float>(y_real.size());
+            }
+        }
+        return avg_gradients;
+    }
+
+};
+
+#endif //IAHOMEWORK_MLP_H
